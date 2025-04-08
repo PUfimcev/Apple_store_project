7421b7aabf7fb8c2441348a6ec0dd24324edb40c
Pavel <u_pasha@icloud.com>
2025-04-08 22:25:47 +0300

    Installed VueJs into laravel and default authorization via Vue. Made the first MainComponent.vue and launched it

---
+++ resources/js/components/MainComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+
+                    <div class="card-body">
+                        I'm an Main component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MainComponent',
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
