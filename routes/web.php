a9ec780afd95aa8348d561dc7e2ecf88f3892d3f
Pavel <u_pasha@icloud.com>
2025-04-19 14:24:07 +0300

    Made settings of congiguration files fot auth and filament

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+Auth::routes();
+
+Route::middleware(['auth:admin'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
 Route::get('/', function () {
     return view('root');
 });
 
-Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
