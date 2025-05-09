049782bd49ec008fb090aaabc136a47f1d2b6504
Pavel <u_pasha@icloud.com>
2025-05-09 23:35:38 +0300

    Installed axios, global base_url for axios, bootstrap-vue-next

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Components from 'unplugin-vue-components/vite'
+import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
+// import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
+    // tailwindcss(),
   ],
   resolve: {
     alias: {
