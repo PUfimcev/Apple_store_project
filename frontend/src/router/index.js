f5ecc8648f3ff69b36aec0c00c3c9e13bbb4be9b
Pavel <u_pasha@icloud.com>
2025-05-13 19:51:13 +0300

    removed excessive code

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainView from '../views/pages/main/MainView.vue'
+
 import navBarRoutes from "@/router/navBarRoutes.js";
 
 const router = createRouter({
